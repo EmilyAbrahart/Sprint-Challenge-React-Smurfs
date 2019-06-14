d12b269b2f56d7bcc9436270092ee22e71b2f314
EmilyAbrahart <emilyabrahart@gmail.com>
2019-06-14 09:39:02 +0100

    Form styling and buttons added

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -2,34 +2,57 @@ import React from 'react';
 import styled from 'styled-components';
 
 const SmurfDiv = styled.div`
-width: 500px;
-height: 200px;
-margin: 2rem;
-color: white;
-border-radius: 2rem;
-border: 4px solid white;
-background: rgba(255,255,255,0.2);
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-`
+	width: 500px;
+	height: 200px;
+	margin: 2rem;
+	color: white;
+	border-radius: 2rem;
+	border: 4px solid white;
+	background: rgba(255, 255, 255, 0.2);
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+`;
+const SmurfButtonContainer = styled.div`
+	display: flex;
+`;
+const SmurfButton = styled.button`
+	text-decoration: none;
+	background: white;
+	color: #00a9d2;
+	border: 2px solid #00a9d2;
+	padding: 0.5rem 2rem;
+	border-radius: 1rem;
+	margin-bottom: 1rem;
+	font-weight: bold;
+	border: 2px solid #00a9d2;
+
+	&:hover {
+		background: #00a9d2;
+		color: white;
+		border: 2px solid white;
+	}
+`;
 
 const Smurf = props => {
-  return (
-    <SmurfDiv>
-      <h3>{props.name}</h3>
-      <strong>{props.height} tall</strong>
-      <p>{props.age} smurf years old</p>
-    </SmurfDiv>
-  );
+	return (
+		<SmurfDiv>
+			<h3>{props.name}</h3>
+			<strong>{props.height} tall</strong>
+			<p>{props.age} smurf years old</p>
+			<SmurfButtonContainer>
+				<SmurfButton>Update</SmurfButton>
+				<SmurfButton>Delete</SmurfButton>
+			</SmurfButtonContainer>
+		</SmurfDiv>
+	);
 };
 
 Smurf.defaultProps = {
-  name: '',
-  height: '',
-  age: ''
+	name: '',
+	height: '',
+	age: ''
 };
 
 export default Smurf;
-
