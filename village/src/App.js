7593eec86dcfd59303594f3ebd33dc0ea5059b3a
EmilyAbrahart <emilyabrahart@gmail.com>
2019-06-14 08:57:01 +0100

    React Router setup

--- village/src/App.js
+++ village/src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import axios from 'axios'
-
+import axios from 'axios';
+import { Route, NavLink } from 'react-router-dom';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      smurfs: [],
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			smurfs: []
+		};
+	}
 
-  getSmurfs = () => {
+	getSmurfs = () => {
 		axios
 			.get('http://localhost:3333/smurfs')
 			.then(res =>
@@ -26,9 +26,9 @@ class App extends Component {
 
 	componentDidMount() {
 		this.getSmurfs();
-  }
-  
-  addNewSmurf = props => {
+	}
+
+	addNewSmurf = props => {
 		axios
 			.post('http://localhost:3333/smurfs', props)
 			.then(res =>
@@ -38,17 +38,34 @@ class App extends Component {
 			)
 			.catch(err => console.log(err));
 	};
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
-  render() {
-    return (
-      <div className="App">
-        <SmurfForm addNewSmurf={this.addNewSmurf }/>
-        <Smurfs smurfs={this.state.smurfs} />
-      </div>
-    );
-  }
+	// add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
+	// Notice what your map function is looping over and returning inside of Smurfs.
+	// You'll need to make sure you have the right properties on state and pass them down to props.
+	render() {
+		return (
+			<div className="App">
+				<nav>
+					<NavLink exact to="/" activeClassName="activeNavButton">
+						Smurf Village
+					</NavLink>
+					<NavLink to="/smurf-form" activeClassName="activeNavButton">
+						Add A Smurf
+					</NavLink>
+				</nav>
+				<Route
+					path="/smurf-form"
+					render={props => (
+						<SmurfForm {...props} addNewSmurf={this.addNewSmurf} />
+					)}
+				/>
+				<Route
+					exact
+					path="/"
+					render={props => <Smurfs {...props} smurfs={this.state.smurfs} />}
+				/>
+			</div>
+		);
+	}
 }
 
 export default App;
